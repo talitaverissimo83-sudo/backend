0f7779acbaa0c6de2fd829857de8944a47264d2d
Aluno <talita.verissimo.83@gmail.com>
2026-09-18 12:00:31 -0300

    java dados

--- AULA2/exercicios_01.js
+++ AULA2/exercicios_01.js
@@ -7,3 +7,19 @@ var anoNascimento = 2009;
 var idade = anoAtual - anoNascimento;
 
 console.log(idade);
+
+// Template String
+console.log('Sua idade é: ${idade} anos.')
+
+// Boolean (Verdadeiro ou falso)
+// Um boolean pode ter apenas dois valores: true (verdadeiro) ou false (falso)
+
+let maiorDeIdade = true;
+let menorDeIdade = false; 
+
+console.log(maiorDeIdade); //exibe true
+console.log(menorDeIdade); //exibe False
+
+let idade = 16;
+let podeDirigir = idade >= 18;
+console.log(podeDirigir);
