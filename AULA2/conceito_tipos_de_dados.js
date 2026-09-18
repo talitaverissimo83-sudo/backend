93fda5af14c2a250abda45183cbd5491584d2172
Aluno <talita.verissimo.83@gmail.com>
2026-09-18 11:16:58 -0300

    exercicio java

--- AULA2/conceito_tipos_de_dados.js
+++ AULA2/conceito_tipos_de_dados.js
@@ -9,3 +9,23 @@
 // Array (Listas de valores)
 // Null (valor vazio)
 // Underfined (quando algo não foi definido)
+
+// STRING (Textos)
+// Uma String é um texto, sempre escrito entre aspas ("" ou '')
+let nome = "Jarvis";
+let mensagem = 'Olá, mundo!'
+
+console.log(nome);
+console.log(mensagem);
+
+let saudacao = "Olá, " + "!";
+console.log(saudacao); //exibe "Olá, Jarvis"
+
+// typeof
+// typeof serve para descobrir o tipo de um valor ou variável 
+
+let nomeDois = "2"
+console.log(typeof nomeDois);
+
+let soma = 10 + 5
+
---
+++ AULA2/exercicios_01.js
@@ -0,0 +1,9 @@
+// Atividade Number 
+// crie uma variável chama anoAtual e outra chamada anoNascimento
+// calcule e exiba a idade no(a) console
+
+var anoAtual = 2026;
+var anoNascimento = 2005;
+var idade = anoAtual - anoNascimento;
+
+console.log(idade);
